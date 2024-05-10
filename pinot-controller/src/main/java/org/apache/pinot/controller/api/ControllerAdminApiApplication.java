5f62cf5359597f6a7601c0ec50fcd57d54df5df9
sar12332111 <160210068+sar12332111@users.noreply.github.com>
2024-05-10 12:11:10 -0700

    Update ControllerAdminApiApplication.java

--- pinot-controller/src/main/java/org/apache/pinot/controller/api/ControllerAdminApiApplication.java
+++ pinot-controller/src/main/java/org/apache/pinot/controller/api/ControllerAdminApiApplication.java
@@ -1,24 +1,17 @@
-/**
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-package org.apache.pinot.controller.api;
+import org.apache.pinot.controller.ControllerConf;
+import org.apache.pinot.core.transport.ListenerConfig;
+import org.apache.pinot.core.util.ListenerConfigUtil;
+import org.apache.pinot.spi.utils.CommonConstants;
+import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
+import org.glassfish.grizzly.http.server.HttpHandler;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.grizzly.http.server.Request;
+import org.glassfish.grizzly.http.server.Response;
+import org.glassfish.hk2.utilities.binding.AbstractBinder;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+import org.glassfish.jersey.server.ResourceConfig;
 
-import io.swagger.jaxrs.config.BeanConfig;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -27,126 +20,105 @@ import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
-import org.apache.pinot.controller.ControllerConf;
-import org.apache.pinot.controller.api.access.AuthenticationFilter;
-import org.apache.pinot.core.api.ServiceAutoDiscoveryFeature;
-import org.apache.pinot.core.transport.ListenerConfig;
-import org.apache.pinot.core.util.ListenerConfigUtil;
-import org.apache.pinot.spi.utils.CommonConstants;
-import org.apache.pinot.spi.utils.PinotReflectionUtils;
-import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.hk2.utilities.binding.AbstractBinder;
-import org.glassfish.jersey.jackson.JacksonFeature;
-import org.glassfish.jersey.media.multipart.MultiPartFeature;
-import org.glassfish.jersey.server.ResourceConfig;
-
 
 public class ControllerAdminApiApplication extends ResourceConfig {
-  public static final String PINOT_CONFIGURATION = "pinotConfiguration";
+    private final String _controllerResourcePackages;
+    private final boolean _useHttps;
+    private HttpServer _httpServer;
 
-  public static final String START_TIME = "controllerStartTime";
+    public ControllerAdminApiApplication(ControllerConf conf) {
+        super();
+        property("pinotConfiguration", conf);
 
-  private final String _controllerResourcePackages;
-  private final boolean _useHttps;
-  private HttpServer _httpServer;
-
-  public ControllerAdminApiApplication(ControllerConf conf) {
-    super();
-    property(PINOT_CONFIGURATION, conf);
-
-    _controllerResourcePackages = conf.getControllerResourcePackages();
-    packages(_controllerResourcePackages);
-    // TODO See ControllerResponseFilter
-    // register(new LoggingFeature());
-    _useHttps = Boolean.parseBoolean(conf.getProperty(ControllerConf.CONSOLE_SWAGGER_USE_HTTPS));
-    if (conf.getProperty(CommonConstants.Controller.CONTROLLER_SERVICE_AUTO_DISCOVERY, false)) {
-      register(ServiceAutoDiscoveryFeature.class);
+        _controllerResourcePackages = conf.getControllerResourcePackages();
+        packages(_controllerResourcePackages);
+        _useHttps = Boolean.parseBoolean(conf.getProperty(ControllerConf.CONSOLE_SWAGGER_USE_HTTPS));
+        if (conf.getProperty(CommonConstants.Controller.CONTROLLER_SERVICE_AUTO_DISCOVERY, false)) {
+            register(ServiceAutoDiscoveryFeature.class);
+        }
+        register(JacksonFeature.class);
+        register(MultiPartFeature.class);
+        registerClasses(io.swagger.jaxrs.listing.ApiListingResource.class, io.swagger.jaxrs.listing.SwaggerSerializers.class);
+        register(new CorsFilter());
+        register(AuthenticationFilter.class);
     }
-    register(JacksonFeature.class);
-    register(MultiPartFeature.class);
-    registerClasses(io.swagger.jaxrs.listing.ApiListingResource.class);
-    registerClasses(io.swagger.jaxrs.listing.SwaggerSerializers.class);
-    register(new CorsFilter());
-    register(AuthenticationFilter.class);
-    // property("jersey.config.server.tracing.type", "ALL");
-    // property("jersey.config.server.tracing.threshold", "VERBOSE");
-  }
 
-  public void registerBinder(AbstractBinder binder) {
-    register(binder);
-  }
-
-  public void start(List<ListenerConfig> listenerConfigs) {
-    _httpServer = ListenerConfigUtil.buildHttpServer(this, listenerConfigs);
-
-    try {
-      _httpServer.start();
-    } catch (IOException e) {
-      throw new RuntimeException("Failed to start http server", e);
+    public void registerBinder(AbstractBinder binder) {
+        register(binder);
     }
-    PinotReflectionUtils.runWithLock(this::setupSwagger);
 
-    ClassLoader classLoader = ControllerAdminApiApplication.class.getClassLoader();
+    public void start(List<ListenerConfig> listenerConfigs) {
+        _httpServer = ListenerConfigUtil.buildHttpServer(this, listenerConfigs);
 
-    // This is ugly from typical patterns to setup static resources but all our APIs are
-    // at path "/". So, configuring static handler for path "/" does not work well.
-    // Configuring this as a default servlet is an option but that is still ugly if we evolve
-    // So, we setup specific handlers for static resource directory. index.html is served directly
-    // by a jersey handler
+        try {
+            _httpServer.start();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to start http server", e);
+        }
+        setupSwagger();
 
-    _httpServer.getServerConfiguration()
-        .addHttpHandler(new CLStaticHttpHandler(classLoader, "/webapp/"), "/index.html");
-    _httpServer.getServerConfiguration()
-        .addHttpHandler(new CLStaticHttpHandler(classLoader, "/webapp/images/"), "/images/");
-    _httpServer.getServerConfiguration().addHttpHandler(new CLStaticHttpHandler(classLoader, "/webapp/js/"), "/js/");
-  }
-
-  private void setupSwagger() {
-    BeanConfig beanConfig = new BeanConfig();
-    beanConfig.setTitle("Pinot Controller API");
-    beanConfig.setDescription("APIs for accessing Pinot Controller information");
-    beanConfig.setContact("https://github.com/apache/pinot");
-    beanConfig.setVersion("1.0");
-    beanConfig.setExpandSuperTypes(false);
-    if (_useHttps) {
-      beanConfig.setSchemes(new String[]{CommonConstants.HTTPS_PROTOCOL});
-    } else {
-      beanConfig.setSchemes(new String[]{CommonConstants.HTTP_PROTOCOL, CommonConstants.HTTPS_PROTOCOL});
+        ClassLoader classLoader = ControllerAdminApiApplication.class.getClassLoader();
+        _httpServer.getServerConfiguration().addHttpHandler(
+            new SafeStaticHttpHandler(classLoader, "/webapp/"), "/index.html");
+        _httpServer.getServerConfiguration().addHttpHandler(
+            new SafeStaticHttpHandler(classLoader, "/webapp/images/"), "/images/");
+        _httpServer.getServerConfiguration().addHttpHandler(
+            new SafeStaticHttpHandler(classLoader, "/webapp/js/"), "/js/");
     }
-    beanConfig.setBasePath("/");
-    beanConfig.setResourcePackage(_controllerResourcePackages);
-    beanConfig.setScan(true);
 
-    ClassLoader loader = this.getClass().getClassLoader();
-    CLStaticHttpHandler apiStaticHttpHandler = new CLStaticHttpHandler(loader, "/api/");
-    // map both /api and /help to swagger docs. /api because it looks nice. /help for backward compatibility
-    _httpServer.getServerConfiguration().addHttpHandler(apiStaticHttpHandler, "/api/");
-    _httpServer.getServerConfiguration().addHttpHandler(apiStaticHttpHandler, "/help/");
+    private void setupSwagger() {
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setTitle("Pinot Controller API");
+        beanConfig.setDescription("APIs for accessing Pinot Controller information");
+        beanConfig.setContact("https://github.com/apache/pinot");
+        beanConfig.setVersion("1.0");
+        beanConfig.setSchemes(new String[]{_useHttps ? CommonConstants.HTTPS_PROTOCOL : CommonConstants.HTTP_PROTOCOL});
+        beanConfig.setBasePath("/");
+        beanConfig.setResourcePackage(_controllerResourcePackages);
+        beanConfig.setScan(true);
 
-    URL swaggerDistLocation = loader.getResource(CommonConstants.CONFIG_OF_SWAGGER_RESOURCES_PATH);
-    CLStaticHttpHandler swaggerDist = new CLStaticHttpHandler(new URLClassLoader(new URL[]{swaggerDistLocation}), "swagger-ui/");
-    _httpServer.getServerConfiguration().addHttpHandler(swaggerDist, "/swaggerui-dist/");
-  }
+        ClassLoader loader = this.getClass().getClassLoader();
+        URL swaggerDistLocation = loader.getResource(CommonConstants.CONFIG_OF_SWAGGER_RESOURCES_PATH);
+        HttpHandler swaggerUiHandler = new SafeStaticHttpHandler(new URLClassLoader(new URL[]{swaggerDistLocation}), "/");
 
-  public void stop() {
-    if (!_httpServer.isStarted()) {
-      return;
+        _httpServer.getServerConfiguration().addHttpHandler(swaggerUiHandler, "/api/");
+        _httpServer.getServerConfiguration().addHttpHandler(swaggerUiHandler, "/help/");
+        _httpServer.getServerConfiguration().addHttpHandler(swaggerUiHandler, "/swaggerui-dist/");
     }
-    _httpServer.shutdownNow();
-  }
 
-  private class CorsFilter implements ContainerResponseFilter {
-    @Override
-    public void filter(ContainerRequestContext containerRequestContext,
-        ContainerResponseContext containerResponseContext)
-        throws IOException {
-      containerResponseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
-      containerResponseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, DELETE");
-      containerResponseContext.getHeaders().add("Access-Control-Allow-Headers", "*");
-      if (containerRequestContext.getMethod().equals("OPTIONS")) {
-        containerResponseContext.setStatus(HttpServletResponse.SC_OK);
-      }
+    public void stop() {
+        if (!_httpServer.isStarted()) {
+            return;
+        }
+        _httpServer.shutdownNow();
     }
-  }
+
+    private class CorsFilter implements ContainerResponseFilter {
+        @Override
+        public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) {
+            responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+            responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, DELETE");
+            responseContext.getHeaders().add("Access-Control-Allow-Headers", "*");
+            if (requestContext.getMethod().equals("OPTIONS")) {
+                responseContext.setStatus(HttpServletResponse.SC_OK);
+            }
+        }
+    }
+
+    public class SafeStaticHttpHandler extends CLStaticHttpHandler {
+        public SafeStaticHttpHandler(ClassLoader classLoader, String... docRoots) {
+            super(classLoader, docRoots);
+        }
+
+        @Override
+        public void service(Request request, Response response) throws Exception {
+            String uri = request.getRequestURI();
+            if (uri.contains("../") || uri.contains("://") || uri.contains("%")) {
+                response.setStatus(400);
+                response.getWriter().write("Invalid request");
+                return;
+            }
+            super.service(request, response);
+        }
+    }
 }
